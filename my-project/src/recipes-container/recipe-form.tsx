ea5198eadf8b16b99bd8f697b9b84b21cceda808
teodoratsaneva <teodora.tsaneva@p3-group.com>
2024-01-24 18:29:54 +0200

    changing cook button

--- my-project/src/recipes-container/recipe-form.tsx
+++ my-project/src/recipes-container/recipe-form.tsx
@@ -1,23 +1,28 @@
 import IngredientsListComponent from "./recipe-list";
 import { Link } from "react-router-dom";
 import { Recipe } from "../interfaces/recipe-interface";
+import Button from "@mui/material/Button";
 
 const RecipeComponent = (props: { recipe: Recipe }) => {
-    const { recipe } = props;
+	const { recipe } = props;
 
-    return (
-        <div className="recipe-card">
-            <h2 className="header-card">{recipe.title}</h2>
-            <IngredientsListComponent ingredients={recipe.ingredients} />
-            <div className="footer-card">
-                <Link to="/cook">
-                    <button key={recipe.id} className="cook-button">
-                        Cook
-                    </button>
-                </Link>
-            </div>
-        </div>
-    );
+	return (
+		<div className="recipe-card">
+			<h2 className="header-card">{recipe.title}</h2>
+			<IngredientsListComponent ingredients={recipe.ingredients} />
+			<div className="footer-card">
+				<Button
+					key={recipe.id}
+					className="cook-button"
+					variant="contained"
+					component={Link}
+					to="/cook"
+				>
+					Cook
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default RecipeComponent;
