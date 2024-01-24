61ad1104d20d0a017e72f48678a154cdb44bff0c
teodoratsaneva <teodora.tsaneva@p3-group.com>
2024-01-24 18:55:45 +0200

    creating modal

--- my-project/src/pages/home-page.tsx
+++ my-project/src/pages/home-page.tsx
@@ -2,8 +2,28 @@ import { Link } from "react-router-dom";
 import HeaderComponent from "../utils/header-page-component";
 import Button from "@mui/material/Button";
 import Heading from "../utils/heading-component";
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { useState } from "react";
+
+const style = {
+	position: 'absolute' as 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	bgcolor: 'background.paper',
+	border: '2px solid #000',
+	boxShadow: 24,
+	p: 4,
+  };
 
 const HomePage = () => {
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
 	return (
 		<>
 			<HeaderComponent />
@@ -14,11 +34,20 @@ const HomePage = () => {
 				<Button
 					className="home-page-button"
 					variant="contained"
-					component={Link}
-					to="/create-cooking-spells"
+					onClick={handleOpen}
 				>
 					Create a cooking spell
 				</Button>
+				<Modal
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description"
+				>
+					<Box sx={style}>
+						
+					</Box>
+				</Modal>
 				<Button
 					className="home-page-button"
 					variant="contained"
