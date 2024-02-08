090f2767e3da9597362a0ce407a3ab7885866fd8
teodoratsaneva <teodora.tsaneva@p3-group.com>
2024-02-08 12:11:34 +0200

    reforming structure of code

--- my-project/src/cook-page-components/cook-arena.tsx
+++ my-project/src/cook-page-components/cook-arena.tsx
@@ -1,109 +1,161 @@
 import p5 from "p5";
 import { useEffect } from "react";
 
+interface Pot {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    image: p5.Image;
+}
+
+interface Ingredient {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    speedY: number;
+    isVisible: boolean;
+    image: p5.Image;
+    update(): void;
+    display(): void;
+    reset(): void;
+    collidesWith(otherObject: { x: number; y: number; width: number; height: number }): boolean;
+}
+
 const CookArena = () => {
     useEffect(() => {
-        const setup = (p) => {
-            let potImg;
-            let carrotImg;
+        const cookArenaSketch = (p: p5) => {
+            let pot: Pot;
+            const ingredients: Ingredient[] = [];
             const segLength = 18;
-            let ingredient;
+
+            class Pot implements Pot {
+                x: number;
+                y: number;
+                width: number;
+                height: number;
+                image: p5.Image;
+
+                constructor(x: number, image: p5.Image) {
+                    this.x = x;
+                    this.y = 850;
+                    this.width = 200;
+                    this.height = 200;
+                    this.image = image;
+                }
+
+                dragSegment = (i: number, xin: number) => {
+                    xin = p.constrain(xin, 0, p.width - 200);
+                    const dx = xin - p.width / 2;
+                    const angle = p.atan2(0, dx);
+                    this.x = xin - p.cos(angle) * segLength;
+                    this.display();
+                };
+
+                display() {
+                    p.image(this.image, this.x, 850, 200, 200);
+                }
+            }
+
+            class Ingredient implements Ingredient {
+                x: number;
+                y: number;
+                width: number;
+                height: number;
+                speedY: number;
+                isVisible: boolean;
+                image: p5.Image;
+
+                constructor(x: number, y: number, image: p5.Image) {
+                    this.x = x;
+                    this.y = y;
+                    this.width = 90;
+                    this.height = 90;
+                    this.speedY = 4;
+                    this.isVisible = true;
+                    this.image = image;
+                }
+
+                update() {
+                    if (this.isVisible) {
+                        this.y += this.speedY;
+                        if (this.y > p.height) {
+                            this.reset();
+                        }
+                    }
+                }
+
+                display() {
+                    if (this.isVisible) {
+                        p.image(this.image, this.x, this.y, this.width, this.height);
+                    }
+                }
+
+                reset() {
+                    this.y = p.random(-50, 0);
+                    this.x = p.random(0, p.width);
+                }
+
+                collidesWith(otherObject: { x: number; y: number; width: number; height: number }) {
+                    if (
+                        otherObject.x < this.x &&
+                        otherObject.x + otherObject.width > this.x &&
+                        otherObject.y < this.y + this.height &&
+                        otherObject.x + otherObject.width > this.x ||
+                        (otherObject.x < this.x + this.width &&
+                            otherObject.x + otherObject.width > this.x + this.width &&
+                            otherObject.y > this.y + this.height &&
+                            otherObject.y + otherObject.height < this.y + this.height)
+                    ) {
+                        this.isVisible = false;
+
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
 
             p.setup = () => {
                 p.createCanvas(1330, 1069);
                 p.strokeWeight(9);
                 p.stroke(255, 100);
-                potImg = {
-                    image: p.loadImage("./pot.png"),
-                    xPos: 0 // Starting position, can be adjusted
-                };
-                carrotImg = p.loadImage("./carrot-svgrepo-com.svg");
-                ingredient = new fallSegment();
+
+                const potImage = p.loadImage("./pot.png");
+                pot = new Pot(p.width / 2, potImage);
+
+                const ingredientsImages = [
+                    p.loadImage("./carrot-svgrepo-com.svg"),
+                    p.loadImage("./cheese-1-svgrepo-com.svg"),
+                    p.loadImage("./butter-svgrepo-com.svg"),
+                    p.loadImage("./egg-svgrepo-com.svg")
+                ];
+
+                for (let i = 0; i < ingredientsImages.length; i++) {
+                    const x = p.random(0, p.width);
+                    const y = p.random(-50, 0);
+                    const ingredient = new Ingredient(x, y, ingredientsImages[i]);
+                    ingredients.push(ingredient);
+                }
             };
 
             p.draw = () => {
                 p.clear();
                 p.background(255, 255, 255, 0);
-                p.dragSegment(0, p.mouseX);
-                ingredient.update();
-                ingredient.display();
 
-                if (ingredient.isVisible && ingredient.collidesWithPot(potImg)) {
-                    console.log("Collision detected!");
-                } else {
-                    ingredient.update();
-                    ingredient.display();
-                }
-            };
+                pot.dragSegment(0, p.mouseX);
 
-            p.dragSegment = (i, xin) => {
-                xin = p.constrain(xin, 0, p.width - 200);
-                const dx = xin - p.width / 2;
-                const angle = p.atan2(0, dx);
-                potImg.xPos = xin - p.cos(angle) * segLength;
-                p.segment(potImg.xPos, 850); // Using potImg.xPos as the x position
-            };
-
-            p.segment = (x, y) => {
-                p.image(potImg.image, x, y, 200, 200);
-            };
-
-            const fallSegment = function () {
-                this.posX = p.random(0, p.width);
-                this.posY = p.random(-50, 0);
-                this.speedY = 2;
-                this.isVisible = true;
-
-                this.update = function () {
-                    if (this.isVisible) {
-                        this.posY += this.speedY;
-                        if (this.posY > p.height) {
-                            this.posY = p.random(-50, 0);
-                            this.posX = p.random(0, p.width);
-                        }
+                ingredients.forEach(ingredient => {
+                    if (!(ingredient.isVisible && ingredient.collidesWith(pot))) {
+                        ingredient.update();
+                        ingredient.display();
                     }
-                };
-
-                this.display = function () {
-                    if (this.isVisible) {
-                        p.image(carrotImg, this.posX, this.posY, 90, 90);
-                    }
-                };
-
-                this.collidesWithPot = function (potImg) {
-                    const potBox = {
-                        x: potImg.xPos,
-                        y: 850,
-                        width: 200,
-                        height: 200
-                    };
-                    const carrotBox = {
-                        x: this.posX,
-                        y: this.posY,
-                        width: 90,
-                        height: 90
-                    };
-
-                    if (
-                        potBox.x < carrotBox.x &&
-                        potBox.x + potBox.width > carrotBox.x &&
-                        potBox.y < carrotBox.y + carrotBox.height &&
-                        potBox.x + potBox.width > carrotBox.x ||
-                        (potBox.x < carrotBox.x + carrotBox.width &&
-                            potBox.x + potBox.width > carrotBox.x + carrotBox.width &&
-                            potBox.y > carrotBox.y + carrotBox.height &&
-                            potBox.y + potBox.height < carrotBox.y + carrotBox.height)
-                    ) {
-                        this.isVisible = false;
-                        return true;
-                    } else {
-                        return false;
-                    }
-                };
+                });
             };
         };
 
-        new p5(setup, document.getElementsByClassName("arena")[0] as HTMLElement);
+        new p5(cookArenaSketch, document.querySelector(".arena") as HTMLElement);
     }, []);
 
     return <div className="arena"></div>;
