f203d4de41d5a9fc385f891df4209b78f3a46ceb
teodoratsaneva <teodora.tsaneva@p3-group.com>
2024-02-14 18:56:32 +0200

    adding last changes

--- my-project/src/cook-page-components/cook-arena.tsx
+++ my-project/src/cook-page-components/cook-arena.tsx
@@ -5,144 +5,171 @@ import { IngredientSeg } from "./ingredient-seg";
 import { ingredientIconMap } from "../utils/ingredients-icons";
 import { Ingredient } from "../interfaces/ingredient-interface";
 
-const CookArena = (props: { ingredients: Ingredient[]; onCatch; checkDiscoMode}) => {
-    const { ingredients, onCatch, checkDiscoMode } = props;
-    const imgWidth = 90;
-    const imgHeight = 90;
-    const x = 0;
-    const y = 0;
-    const canvasHeight = 1035;
-    const canvasWidth = 1330;
+const CookArena = (props: {
+	ingredients: Ingredient[];
+	onCatch;
+	checkDiscoMode;
+	loseOneLive;
+	hearts;
+}) => {
+	const { ingredients, onCatch, checkDiscoMode, loseOneLive, hearts } = props;
+	const imgWidth = 100;
+	const imgHeight = 100;
+	const x = 0;
+	const y = 0;
+	const canvasHeight = 1035;
+	const canvasWidth = 1330;
+	const discoColor = useRef(false);
+	const pausedGame = useRef(false);
+	const allIngredientsCount = ingredients.reduce(
+		(total, ingredient) => total + parseInt(ingredient.quantity),
+		0
+	);
+	const ingredientsCount = useRef(allIngredientsCount);
 
-    const discoColor = useRef(false);
+	useEffect(() => {
+		const cookArenaSketch = (p: p5) => {
+			const potImage = p.loadImage("./pot.png");
+			const pot = new Pot(p.width / 2, potImage, p);
+			const ingredientsSeg: IngredientSeg[] = [];
+			const invalidIngredient = {
+				id: "1",
+				quantity: "1",
+				title: "Cat",
+			};
 
-    useEffect(() => {
-        const cookArenaSketch = (p: p5) => {
-            let pot: Pot;
-            const ingredientsSeg: IngredientSeg[] = [];
-            const invalidIngredient = {
-                id: "1",
-                quantity: "1",
-                title: "Cat",
-            };
+			p.setup = () => {
+				p.createCanvas(1330, 1035);
+				p.strokeWeight(9);
+				p.stroke(255, 100);
+				p.background(255, 255, 255, 0);
 
-            p.setup = () => {
-                p.createCanvas(1330, 1035);
-                p.strokeWeight(9);
-                p.stroke(255, 100);
-                p.background(255, 255, 255, 0);
+				let countIngredients = 0;
 
-                const potImage = p.loadImage("./pot.png");
-                pot = new Pot(p.width / 2, potImage, p);
+				const spawnIngredient = () => {
+					if (countIngredients < ingredients.length + 1) {
+						const valueOfIng = ingredients[countIngredients].title;
+						const ingredientQuantity = parseInt(
+							ingredients[countIngredients].quantity
+						);
 
-                let countIngredients = 0;
+						let countQuantity = 0;
 
-                const spawnIngredient = () => {
-                    if (countIngredients < ingredients.length + 1) {
-                        const valueOfIng = ingredients[countIngredients].title;
-                        const ingredientQuantity = parseInt(
-                            ingredients[countIngredients].quantity
-                        );
+						const spawnSingleIngredient = () => {
+							if (countQuantity < ingredientQuantity) {
+								const ingredient: Ingredient =
+									ingredients[countIngredients];
 
-                        let countQuantity = 0;
+								const ingredientSeg = new IngredientSeg(
+									x,
+									y,
+									imgWidth,
+									imgHeight,
+									canvasWidth,
+									canvasHeight,
+									p.loadImage(ingredientIconMap[valueOfIng]),
+									p,
+									ingredient
+								);
 
-                        const spawnSingleIngredient = () => {
-                            if (countQuantity < ingredientQuantity) {
-                                const ingredient: Ingredient =
-                                    ingredients[countIngredients];
+								ingredientSeg.reset();
+								ingredientsSeg.push(ingredientSeg);
+								countQuantity++;
 
-                                const ingredientSeg = new IngredientSeg(
-                                    x,
-                                    y,
-                                    imgWidth,
-                                    imgHeight,
-                                    canvasWidth,
-                                    canvasHeight,
-                                    p.loadImage(ingredientIconMap[valueOfIng]),
-                                    p,
-                                    ingredient
-                                );
+								setTimeout(spawnSingleIngredient, 2000);
+							} else {
+								countIngredients++;
+								setTimeout(spawnIngredient, 2000);
+							}
+						};
 
-                                ingredientSeg.reset();
-                                ingredientsSeg.push(ingredientSeg);
-                                countQuantity++;
+						spawnSingleIngredient();
+					}
+				};
 
-                                setTimeout(spawnSingleIngredient, 2000);
-                            } else {
-                                countIngredients++;
-                                setTimeout(spawnIngredient, 2000);
+				setTimeout(spawnIngredient, 2000);
+
+				const spawnInvalidIngredient = () => {
+					const catImage =
+						p.round(p.random(1, 2)) === 1
+							? "./cat.png"
+							: "./cute-cat.png";
+
+					const invalidSeg = new IngredientSeg(
+						x,
+						y,
+						imgWidth,
+						imgHeight,
+						canvasWidth,
+						canvasHeight,
+						p.loadImage(catImage),
+						p,
+						invalidIngredient
+					);
+
+					invalidSeg.reset();
+					ingredientsSeg.push(invalidSeg);
+
+					setTimeout(spawnInvalidIngredient, p.random(5000, 6000));
+				};
+
+				spawnInvalidIngredient();
+			};
+
+			p.draw = () => {
+				if (!pausedGame.current) {
+					p.clear();
+					checkDiscoMode(discoColor.current, p);
+
+					pot.dragSegment(p.mouseX);
+					ingredientsSeg.forEach((ingredient) => {
+						if (
+							!(
+								ingredient.isVisible &&
+								ingredient.collidesWith(pot)
+							)
+						) {
+							ingredient.update();
+							ingredient.display();
+						} else if (
+							ingredient.ingredient === invalidIngredient &&
+							ingredient.collidesWith(pot)
+						) {
+							discoColor.current = true;
+							setTimeout(() => {
+								discoColor.current = false;
+							}, 1000);
+
+							if (hearts === 0) {
+								pausedGame.current = true;
+							}
+
+                            loseOneLive();
+						} else {
+							ingredientsCount.current =
+								ingredientsCount.current - 1;
+
+                            if (ingredientsCount.current === 0) {
+                                pausedGame.current = true;
                             }
-                        };
 
-                        spawnSingleIngredient();
-                    }
-                };
+							onCatch(
+								ingredient.ingredient!.id,
+								ingredientsCount.current
+							);
+						}
+					});
+				}
+			};
+		};
 
-                setTimeout(spawnIngredient, 2000);
+		new p5(
+			cookArenaSketch,
+			document.querySelector(".arena") as HTMLElement
+		);
+	}, []);
 
-                const spawnInvalidIngredient = () => {
-                    // const catImage =
-                    //     p.round(p.random(1, 2)) === 1
-                    //         ? "./cat.png"
-                    //         : "./cutecat.png";
-
-                            const catImage = "./cutecat.png";
-                    //const invalImgHeight = catImage === "./cat.png" ? 130 : 90;
-
-                    const invalidSeg = new IngredientSeg(
-                        x,
-                        y,
-                        imgWidth,
-                        90,
-                        canvasWidth,
-                        canvasHeight,
-                        p.loadImage(catImage),
-                        p,
-                        invalidIngredient
-                    );
-
-                    invalidSeg.reset();
-                    ingredientsSeg.push(invalidSeg);
-
-                    setTimeout(spawnInvalidIngredient, p.random(5000, 6000));
-                };
-
-                spawnInvalidIngredient();
-            };
-
-            p.draw = () => {
-                p.clear();
-                checkDiscoMode(discoColor.current, p);
-
-                pot.dragSegment(p.mouseX);
-                ingredientsSeg.forEach((ingredient) => {
-                    if (
-                        !(ingredient.isVisible && ingredient.collidesWith(pot))
-                    ) {
-                        ingredient.update();
-                        ingredient.display();
-                    } else if (
-                        ingredient.ingredient === invalidIngredient &&
-                        ingredient.collidesWith(pot)
-                    ) {
-                        discoColor.current = true;
-                        setTimeout(() => {
-                            discoColor.current = false;
-                        }, 4000);
-                    } else {
-                        onCatch(ingredient.ingredient.id);
-                    }
-                });
-            };
-        };
-
-        new p5(
-            cookArenaSketch,
-            document.querySelector(".arena") as HTMLElement
-        );
-    }, []); 
-
-    return <div className="arena"></div>;
+	return <div className="arena"></div>;
 };
 
 export default CookArena;
--- my-project/src/pages/cook-page.tsx
+++ my-project/src/pages/cook-page.tsx
@@ -1,47 +1,44 @@
+import { useEffect, useState, useCallback, useRef } from "react";
 import RecipeComponent from "../recipes-container/recipe-form";
 import CookArena from "../cook-page-components/cook-arena";
-import { useEffect, useState, useCallback } from "react";
 import ModalComponent from "../modal-create-recipe/modal-component";
-import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
+import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
 import p5 from "p5";
 
 const CookPage = () => {
 	const activeRecipeRaw = localStorage.getItem("activeRecipe");
 	const activeRecipe = activeRecipeRaw ? JSON.parse(activeRecipeRaw) : null;
-
 	const [open, setOpen] = useState(false);
+	const maxHearts = 3;
+	const [hearts, setHearts] = useState(maxHearts);
+	const [recipe, setRecipe] = useState(activeRecipe);
 
 	const handleOpen = useCallback(() => {
 		setOpen(true);
 	}, []);
+
 	const handleClose = useCallback(() => {
 		setOpen(false);
 	}, []);
 
-	const [recipe, setRecipe] = useState(activeRecipe);
-
-	const onCatch = (id: string) => {
+	const onCatch = (id, ingredientsCount) => {
 		const updatedRecipe = { ...recipe };
 
-		for (let i = 0; i < updatedRecipe.ingredients.length; i++) {
-			if (id === updatedRecipe.ingredients[i].id) {
-				updatedRecipe.ingredients[i].quantity -= 1;
-				setRecipe(updatedRecipe);
+		let i = 0;
+while (i < updatedRecipe.ingredients.length) {
+    if (id === updatedRecipe.ingredients[i].id) {
+        updatedRecipe.ingredients[i].quantity -= 1;
+        setRecipe(updatedRecipe);
+        break;
+    }
+    i++;
+}
 
-				break;
-			}
-		}
 
-		const allIngredientsCatched = updatedRecipe.ingredients.every(
-			(ingredient) => ingredient.quantity === 0
-		);
-
-		if (allIngredientsCatched) {
+		if (ingredientsCount === 0) {
 			handleOpen();
 		}
 	};
-
-	useEffect(() => {}, [recipe]);
 
 	const checkDiscoMode = (discoColor, p: p5) => {
 		if (discoColor) {
@@ -52,7 +49,7 @@ const CookPage = () => {
 				.querySelector(".recipe-cook-page")
 				?.setAttribute(
 					"style",
-					`background-color: ${randomColor()}; color: ${randomColor()} !important;`
+					`background-color: ${randomColor()}; color: ${randomColor()};`
 				);
 			document
 				.querySelector(".recipe-side")
@@ -71,6 +68,22 @@ const CookPage = () => {
 		}
 	};
 
+	const loseOneLive = () => {
+		setHearts(
+			(oldHearts) => {
+				return oldHearts - 1;
+			}
+		);
+	};
+
+	useEffect(() => {
+		if (hearts === 0) {
+			handleOpen();
+		}
+	}, [hearts])
+
+	useEffect(() => {}, [recipe, hearts]);
+
 	return (
 		<div className="cook-page">
 			<div className="cook-page-container">
@@ -78,6 +91,8 @@ const CookPage = () => {
 					ingredients={recipe.ingredients}
 					onCatch={onCatch}
 					checkDiscoMode={checkDiscoMode}
+					loseOneLive={loseOneLive}
+					hearts={hearts}
 				/>
 				<div className="recipe-side">
 					<RecipeComponent
@@ -87,9 +102,12 @@ const CookPage = () => {
 						classNameIngContent="recipe-content-cook-page"
 					/>
 					<div className="heart-lives">
-						<div className="heart-lives">
-								<FavoriteBorderRoundedIcon className="heart"/>
-						</div>
+						{hearts > 0 && [...Array(hearts)].map((_, index) => (
+							<FavoriteBorderRoundedIcon
+								key={index}
+								className="heart"
+							/>
+						))}
 					</div>
 				</div>
 			</div>
