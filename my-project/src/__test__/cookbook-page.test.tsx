9dc8d04926a62598358a120bcbf68c5359efe746
Teodora Tsaneva <teodora.tsaneva@p3-group.com>
2024-03-05 21:58:42 +0200

    adding test for rendering list of recipe component

--- my-project/src/__test__/cookbook-page.test.tsx
+++ my-project/src/__test__/cookbook-page.test.tsx
@@ -2,13 +2,10 @@ import { render } from "@testing-library/react";
 import CookbookPage from "../pages/cookbook-page";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
-import HeaderComponent from "../common-components/header-page-component";
-import RecipeComponent from "../recipe-card/recipe-form";
-import { shallow } from "enzyme";
 
 describe ('CookbookPage', () => {
     it('renders correctly Cookbook page', () => {
-        const tree = render(
+      const tree = render(
 			<BrowserRouter>
 				<CookbookPage />
 			</BrowserRouter>
@@ -29,33 +26,34 @@ describe ('CookbookPage', () => {
         expect(recipesContainers).toHaveLength(0);
     });
 
-    it('should render a list of recipe components', () => {
-        const setStateMock = jest.fn();
-		const useStateMock: any = (useState: any) => [useState, setStateMock];
-		jest.spyOn(React, "useState").mockImplementation(useStateMock);
+    it('should render a list of recipe component', () => {
+      const setStateMock = jest.fn();
+      const useStateMock: any = (useState: any) => [useState, setStateMock];
+      jest.spyOn(React, "useState").mockImplementation(useStateMock);
+    
+      const recipes = [
+        { id: '1', title: 'Recipe 1', ingredients: [] },
+        { id: '2', title: 'Recipe 2', ingredients: [] },
+        { id: '3', title: 'Recipe 3', ingredients: [] },
+      ];
+    
+      const localStorageMock = {
+        getItem: jest.fn(),
+        setItem: jest.fn(),
+        removeItem: jest.fn(),
+      };
 
-        const recipes = [
-          { id: '1', title: 'Recipe 1', ingredients: [] },
-          { id: '2', title: 'Recipe 2', ingredients: [] },
-          { id: '3', title: 'Recipe 3', ingredients: [] },
-        ];
+      global.localStorage = localStorageMock as any;
     
-        const localStorageMock = {
-          getItem: jest.fn(),
-          setItem: jest.fn(),
-          removeItem: jest.fn(),
-        };
-        global.localStorage = localStorageMock as any;
+      localStorageMock.setItem.mockReturnValue(JSON.stringify(recipes));
     
-        localStorageMock.setItem.mockReturnValue(JSON.stringify(recipes));
+      const { getAllByTestId } = render(
+        <BrowserRouter>
+          <CookbookPage />
+        </BrowserRouter>
+      );
     
-        const wrapper = shallow(
-          <BrowserRouter>
-            <CookbookPage />
-          </BrowserRouter>
-        );
-    
-        expect(wrapper.find(HeaderComponent)).toHaveLength(1);
-        expect(wrapper.find(RecipeComponent)).toHaveLength(2);
-      });
+    const recipeComponents = getAllByTestId('recipe-component');
+    expect(recipeComponents).toHaveLength(recipes.length);
+    });    
 })
